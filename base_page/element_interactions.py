9ed9fdcadf5bbb5d9f4538dd88c3ae6ef8d64b4b
Aditya Patel <Aditya.Patel@einfochips.com>
2025-04-22 18:01:50 +0530

    some changes

--- base_page/element_interactions.py
+++ base_page/element_interactions.py
@@ -1,4 +1,3 @@
-from ei_mobly.utils import *
 from .wait_and_synchronization import *
 from .custom_logger import customLogger
 
@@ -6,8 +5,6 @@ from .custom_logger import customLogger
 def get_element(device, locator_type, locator_value, timeout):
     log = customLogger()
     element = None
-    if not get_last_snippet_ui(device):
-        switch_to_ui_snippet(device)
     try:
         element = wait_for_element(device, locator_type, locator_value, timeout)
         log.info(f"Element found with locator type {locator_type} and locator value {locator_value}")
@@ -17,17 +14,13 @@ def get_element(device, locator_type, locator_value, timeout):
 
 
 def get_elements(device, locator_type, locator_value, timeout):
-    # Still to implement
-    # log = customLogger()
-    # element = None
-    # if not get_last_snippet_ui(device):
-    #     switch_to_ui_snippet(device)
-    # try:
-    #     element = wait_for_elements(device, locator_type, locator_value, timeout)
-    #     log.info(f"Element found with locator type {locator_type} and locator value {locator_value}")
-    # except Exception as e:
-    #     log.error(f"Element not found with {locator_type} = {locator_value}. :Error-{e}")
-    # return element
-    pass
+    log = customLogger()
+    elements = None
+    try:
+        elements = wait_for_elements(device, locator_type, locator_value, timeout)
+        log.info(f"Element found with locator type {locator_type} and locator value {locator_value}")
+    except Exception as e:
+        log.error(f"Element not found with {locator_type} = {locator_value}. :Error-{e}")
+    return elements
 
 
