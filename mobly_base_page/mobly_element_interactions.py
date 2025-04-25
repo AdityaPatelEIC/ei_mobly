e971c4ccb0b3b1396470f23e948a0298f2d9fa6b
Aditya Patel <Aditya.Patel@einfochips.com>
2025-04-25 13:13:24 +0530

    added the clear and set text method

--- mobly_base_page/mobly_element_interactions.py
+++ mobly_base_page/mobly_element_interactions.py
@@ -38,10 +38,17 @@ class MoblyElementInteractions:
                 if element.click():
                     log.info(f"Clicked on element with locator type {locator_type} and locator value {locator_value}")
                 else:
-                    log.error(
-                        f"Failed to click on element with locator type {locator_type} and locator value {locator_value}")
+                    log.error(f"Failed to click on element with locator type {locator_type} and locator value {locator_value}")
         except Exception as e:
             log.error(f"Element not found with {locator_type} = {locator_value}. :Error-{e}")
+
+    def click_element_on_devices(self, *args, locator_type, locator_value, timeout):
+        for device in args:
+            self.click_element(device, locator_type, locator_value, timeout)
+
+    def click_element_on_all_devices(self, devices, locator_type, locator_value, timeout):
+        for device in devices:
+            self.click_element(device, locator_type, locator_value, timeout)
 
     def long_click_element(self, device, locator_type, locator_value, timeout):
         log = customLogger()
@@ -49,13 +56,19 @@ class MoblyElementInteractions:
             element = self.wt_syn.wait_for_element(device, locator_type, locator_value, timeout)
             if element is not None:
                 if element.long_click():
-                    log.info(
-                        f"Long clicked on element with locator type {locator_type} and locator value {locator_value}")
+                    log.info(f"Long clicked on element with locator type {locator_type} and locator value {locator_value}")
                 else:
-                    log.error(
-                        f"Failed to long click on element with locator type {locator_type} and locator value {locator_value}")
+                    log.error(f"Failed to long click on element with locator type {locator_type} and locator value {locator_value}")
         except Exception as e:
             log.error(f"Element not found with {locator_type} = {locator_value}. :Error-{e}")
+
+    def long_click_element_on_devices(self, *args, locator_type, locator_value, timeout):
+        for device in args:
+            self.long_click_element(device, locator_type, locator_value, timeout)
+
+    def long_click_element_on_all_devices(self, devices, locator_type, locator_value, timeout):
+        for device in devices:
+            self.long_click_element(device, locator_type, locator_value, timeout)
 
     def click_and_hold_element(self, device, locator_type, locator_value, hold_time, timeout):
         _HOLD_WAIT_TIME = datetime.timedelta(seconds=hold_time)
@@ -64,10 +77,54 @@ class MoblyElementInteractions:
             element = self.wt_syn.wait_for_element(device, locator_type, locator_value, timeout)
             if element is not None:
                 if element.click(_HOLD_WAIT_TIME):
-                    log.info(
-                        f"Clicked on element with locator type {locator_type} and locator value {locator_value} for {hold_time} seconds")
+                    log.info(f"Clicked on element with locator type {locator_type} and locator value {locator_value} for {hold_time} seconds")
                 else:
-                    log.error(
-                        f"Failed to click on element with locator type {locator_type} and locator value {locator_value}")
+                    log.error(f"Failed to click on element with locator type {locator_type} and locator value {locator_value}")
         except Exception as e:
             log.error(f"Element not found with {locator_type} = {locator_value}. :Error-{e}")
+
+    def click_and_hold_element_on_devices(self, *args, locator_type, locator_value, hold_time,  timeout):
+        for device in args:
+            self.click_and_hold_element(device, locator_type, locator_value, hold_time,  timeout)
+
+    def click_and_hold_element_on_all_devices(self, devices, locator_type, locator_value, hold_time, timeout):
+        for device in devices:
+            self.click_and_hold_element(device, locator_type, locator_value, hold_time, timeout)
+
+    def set_element_text(self, device, locator_type, locator_value, text, timeout):
+        log = customLogger()
+        try:
+            element = self.wt_syn.wait_for_element(device, locator_type, locator_value, timeout)
+            if element is not None:
+                if element.set_text(text):
+                    log.info(f"Set text = {text} on element with locator type {locator_type} and locator value {locator_value}")
+                else:
+                    log.error(f"Failed to set text {text} on element with locator type {locator_type} and locator value {locator_value}")
+        except Exception as e:
+            log.error(f"Element not found with {locator_type} = {locator_value}. :Error-{e}")
+
+    def set_element_text_on_devices(self, *args, locator_type, locator_value, text, timeout):
+        for device in args:
+            self.set_element_text(device, locator_type, locator_value, text, timeout)
+
+    def set_element_text_on_all_devices(self, devices, locator_type, locator_value, text, timeout):
+        for device in devices:
+            self.set_element_text(device, locator_type, locator_value, text, timeout)
+
+    def clear_element_text(self, device, locator_type, locator_value, text, timeout):
+        log = customLogger()
+        try:
+            element = self.wt_syn.wait_for_element(device, locator_type, locator_value, timeout)
+            if element is not None:
+                if element.clear_text(text):
+                    log.info(f"Cleared existing text from the element with locator type {locator_type} and locator "
+                             f"value {locator_value}")
+                else:
+                    log.error(f"Failed to clear existing text from the element with locator type {locator_type} and "
+                              f"locator value {locator_value}")
+        except Exception as e:
+            log.error(f"Element not found with {locator_type} = {locator_value}. :Error-{e}")
+
+    def clear_element_text_on_devices(self, *args, locator_type, locator_value, timeout):
+        for device in args:
+            self.clear_element_text(device, locator_type, locator_value, timeout)
--- mobly_controllers/mobly_element_interactions_controller.py
+++ mobly_controllers/mobly_element_interactions_controller.py
@@ -1,8 +1,10 @@
 from mobly_base_page.mobly_element_interactions import MoblyElementInteractions
 
+
 class ElementInteractionsController:
     def __init__(self):
         self.ele_interaction = MoblyElementInteractions()
+
     def get_element(self, device, locator_type, locator_value, timeout=15):
         return self.ele_interaction.get_element(device, locator_type, locator_value, timeout)
 
@@ -12,8 +14,50 @@ class ElementInteractionsController:
     def click_element(self, device, locator_type, locator_value, timeout=15):
         self.ele_interaction.click_element(device, locator_type, locator_value, timeout)
 
+    def click_element_on_devices(self, *args, locator_type, locator_value, timeout=15):
+        self.ele_interaction.click_element_on_devices(*args, locator_type=locator_type, locator_value=locator_value,
+                                                      timeout=timeout)
+
+    def click_element_on_all_devices(self, devices, locator_type, locator_value, timeout=15):
+        self.ele_interaction.click_element_on_all_devices(devices, locator_type,
+                                                          locator_value, timeout)
+
     def long_click_element(self, device, locator_type, locator_value, timeout=15):
         self.ele_interaction.long_click_element(device, locator_type, locator_value, timeout)
 
+    def long_click_element_on_devices(self, *args, locator_type, locator_value, timeout=15):
+        self.ele_interaction.long_click_element_on_devices(*args, locator_type=locator_type,
+                                                           locator_value=locator_value, timeout=timeout)
+
+    def long_click_element_on_all_devices(self, devices, locator_type, locator_value, timeout=15):
+        self.ele_interaction.long_click_element_on_all_devices(devices, locator_type,
+                                                               locator_value, timeout)
+
     def click_and_hold_element(self, device, locator_type, locator_value, hold_time=2, timeout=15):
         self.ele_interaction.click_and_hold_element(device, locator_type, locator_value, hold_time, timeout)
+
+    def click_and_hold_element_on_devices(self, *args, locator_type, locator_value, hold_time=2, timeout=15):
+        self.ele_interaction.click_and_hold_element_on_devices(*args, locator_type=locator_type,
+                                                               locator_value=locator_value, hold_time=hold_time,
+                                                               timeout=timeout)
+
+    def click_and_hold_element_on_all_devices(self, devices, locator_type, locator_value, hold_time=2, timeout=15):
+        self.ele_interaction.click_and_hold_element_on_all_devices(devices, locator_type, locator_value, hold_time, timeout)
+
+    def set_element_text(self, device, locator_type, locator_value, text, timeout=15):
+        self.ele_interaction.set_element_text(device, locator_type, locator_value, text, timeout)
+
+    def set_element_text_on_devices(self, *args, locator_type, locator_value, text, timeout=15):
+        self.ele_interaction.set_element_text_on_devices(*args, locator_type=locator_type, locator_value=locator_value, text=text, timeout=timeout)
+
+    def set_element_text_on_all_devices(self, devices, locator_type, locator_value, text, timeout=15):
+        self.ele_interaction.set_element_text_on_all_devices(devices, locator_type, locator_value, text, timeout)
+
+    def clear_element_text(self,device, locator_type, locator_value, timeout=15):
+        self.ele_interaction.clear_element_text(device, locator_type, locator_value, timeout)
+
+    def clear_element_text_on_devices(self, *args, locator_type, locator_value, timeout=15):
+        self.ele_interaction.clear_element_text_on_devices(*args, locator_type=locator_type, locator_value=locator_value, timeout=timeout)
+
+    def clear_element_text_on_all_devices(self, devices, locator_type, locator_value, timeout=15):
+        self.ele_interaction.clear_element_text_on_devices(devices, locator_type, locator_value, timeout)
